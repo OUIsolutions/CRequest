3cecf6722ceadba22e529d2b7a39434c8a4fc60b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-09-05 10:46:35 -0300

    at

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 int main(){
     CRequestNamespace req = newCRequestNamespace();
     CRequest *r = req.newRequest("google.com.br");
-
+    r->binary_location = "curl";
     req.add_headder(r, "aa", "bbbb");
     req.add_query_parram(r, "aaaaaa", "bbbbb");
     req.set_body_string(r,"aaaaaaaaaaaaaaa");
-    req.represent(r);
+    long size;
+    bool isbinary;
+    char *result = (char*)CRequest_get_any(r,&size,&isbinary);
+    printf("%s",result);
+    
     req.free(r);
 
 
